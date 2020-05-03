c9c657dc85cd81b556ad2d2ba86da58f24db6805
Kunal Dewan <kunalx011@gmail.com>
2020-05-03 17:20:03 +0530

    Updates

---
+++ precache-manifest.a42e44612b6df6357a8764a9a055cecd.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa8f489b9beb643d389418502e0b409",
+    "url": "/BPGC-Remembrance/index.html"
+  },
+  {
+    "revision": "94ff1046d5814681c4ad",
+    "url": "/BPGC-Remembrance/static/css/2.508070a3.chunk.css"
+  },
+  {
+    "revision": "4ffd558f79c9db5316d0",
+    "url": "/BPGC-Remembrance/static/css/main.4ba8389b.chunk.css"
+  },
+  {
+    "revision": "94ff1046d5814681c4ad",
+    "url": "/BPGC-Remembrance/static/js/2.3595e15b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BPGC-Remembrance/static/js/2.3595e15b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ffd558f79c9db5316d0",
+    "url": "/BPGC-Remembrance/static/js/main.b06edb69.chunk.js"
+  },
+  {
+    "revision": "ad4d07b0e587564a3775",
+    "url": "/BPGC-Remembrance/static/js/runtime-main.24c5a805.js"
+  },
+  {
+    "revision": "524a4cbdf53cdb9bb2742d2aa7a25c36",
+    "url": "/BPGC-Remembrance/static/media/ArchivoBlack-Regular.524a4cbd.ttf"
+  },
+  {
+    "revision": "321de678e592d0b8f44f1a82d7ca4b62",
+    "url": "/BPGC-Remembrance/static/media/Roboto-Thin.321de678.ttf"
+  },
+  {
+    "revision": "3a0bafd68b0979dccd57ebc4a482f71d",
+    "url": "/BPGC-Remembrance/static/media/bpgc-parallax.3a0bafd6.jpg"
+  },
+  {
+    "revision": "f52ff57f91db6a216b6692389063674b",
+    "url": "/BPGC-Remembrance/static/media/img1.f52ff57f.jpg"
+  },
+  {
+    "revision": "de744b650d499c5ebb189e3fd75ba99a",
+    "url": "/BPGC-Remembrance/static/media/img2.de744b65.jpg"
+  },
+  {
+    "revision": "1d2969af4392ab369673ee9be661ed8c",
+    "url": "/BPGC-Remembrance/static/media/img3.1d2969af.jpg"
+  },
+  {
+    "revision": "da428f744da5aad4e0af26aa3979b8d2",
+    "url": "/BPGC-Remembrance/static/media/img4.da428f74.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/BPGC-Remembrance/precache-manifest.5525d71f14dfe966215e2214031daf95.js"
+  "/BPGC-Remembrance/precache-manifest.a42e44612b6df6357a8764a9a055cecd.js"
 );
 
 self.addEventListener('message', (event) => {
